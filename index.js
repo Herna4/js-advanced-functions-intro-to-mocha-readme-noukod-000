bb24a0e02d45cb12d711a8f27b6c1d76d76ae8ef
Herna Michel Noël <labellehernamichel@yahoo.fr>
2019-09-23 19:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function sayHey() {
 }
 
 function sayHeyFriend(name) {
+<<<<<<< HEAD
   return `Hey, ${name}!`;
 }
 
@@ -21,4 +22,11 @@ function shouting(name){
 function roundDown(number){
   return Math.floor(number);
   
+=======
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(nameOfIceCream){
+  return `I love ${nameOfIceCream}`
+>>>>>>> 0c5956361d8c42045e9d783182c7740acf27ef4b
 }
